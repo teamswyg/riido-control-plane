95b1508b78eee096fc4ed9483c2a7e7c08fdda02
kimjooyoon <asketeddy@gmail.com>
2026-06-16 23:25:04 +0900

    Opt runtime CD into Node 24 actions

--- tools/deploypolicy/deploy_policy_test.go
+++ tools/deploypolicy/deploy_policy_test.go
@@ -57,6 +57,7 @@ func TestDeployAIAgentTestnetPublicRedactionPolicy(t *testing.T) {
 	requireContains(t, workflow, "TESTNET_BASE_URL: ${{ vars.RIIDO_AI_SERVER_TESTNET_BASE_URL }}")
 	requireContains(t, workflow, "SMOKE_TOKEN_CONFIGURED: ${{ secrets.RIIDO_AI_SERVER_TESTNET_TOKEN != '' }}")
 	requireContains(t, workflow, "if [ \"${SMOKE_TOKEN_CONFIGURED:-false}\" != \"true\" ]")
+	requireContains(t, workflow, "FORCE_JAVASCRIPT_ACTIONS_TO_NODE24: true")
 	requireContains(t, workflow, "CODEDEPLOY_APPLICATION: ${{ vars.RIIDO_AI_SERVER_CODEDEPLOY_APPLICATION }}")
 	requireContains(t, workflow, "CODEDEPLOY_DEPLOYMENT_GROUP: ${{ vars.RIIDO_AI_SERVER_CODEDEPLOY_DEPLOYMENT_GROUP }}")
 	requireContains(t, workflow, "profile-thumbnails/uploads")
